ab366f7c167a7f37b09490c1ea255a14e90f6881
Nedaa Alyasein <155689929+nedaa2024@users.noreply.github.com>
2024-11-05 19:14:39 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿// -------------------Task 01 c#-------------------- 
+// -------------------Task 01 c#-------------------- 
 //1-Write a program that enters the diameter of the circle from the keyboard and prints the circumference of the circle.
+
 Console.WriteLine("We Need to Calc The circumference of the circle so , PLZ ENTER THE DIMETER OF THE CIRCLE : ");
-int diameter= Convert.ToInt32(Console.ReadLine());
-double Result = Math .PI * diameter;
+int diameter = Convert.ToInt32(Console.ReadLine());
+double Result = Math.PI * diameter;
 Console.WriteLine($"circumference of the circle is : {Result} ");
 
 
@@ -24,7 +25,7 @@ int hours = Convert.ToInt32(Console.ReadLine());
 Console.Write("Distance in meters: ");
 double distance = Convert.ToDouble(Console.ReadLine());
 double distance_In_Kilometers = distance / 1000;
-double speed_in_meters_per_hour = distance / hours; 
+double speed_in_meters_per_hour = distance / hours;
 double speed_in_kilometers_per_hour = distance_In_Kilometers / hours;
 Console.WriteLine($"Speed in meters per hour: {speed_in_meters_per_hour}");
 Console.WriteLine($"Speed in kilometers per hour: {speed_in_kilometers_per_hour}");
@@ -32,29 +33,42 @@ Console.WriteLine($"Speed in kilometers per hour: {speed_in_kilometers_per_hour}
 
 
 
+
+Console.WriteLine();
+Console.WriteLine("=====================================================================");
+Console.WriteLine();
+
+
 // 3- What are the differences between Post Increment and Pre-Increment operators?
 
 /*Post-Increment Operator : Increments the variable after its current value is used.
  the result of post increment is  >> The original value is used first, then the variable is incremented.*/
-//let take example : 
+// Post Increment Operator example
 int num = 5;
 int y = num++;
-// y is assigned the value of num (5)  then num is incremented >>> 6
+Console.WriteLine($"Post-Increment Operator:");
+Console.WriteLine($"Initial num = 5");
+Console.WriteLine($"After post-increment: num = {num}, y = {y}");// num = 6, y = 5 
 
 
 /*Pre-Increment Operator : Increments the variable before its new value is used. 
   the result of pre increment is >> The variable is incremented first, then the new value is used.
  */
-//let take example : 
+// Pre Increment Operator example
+
 int num2 = 5;
 int y2 = ++num2;
-//// num2 is incremented to 6 first, then y2 is assigned the new value of num2 (6).
-///
-
+Console.WriteLine($"Pre-Increment Operator:");
+Console.WriteLine($"Initial num2 = 5");
+Console.WriteLine($"After pre-increment: num2 = {num2}, y2 = {y2}"); // num2 = 6, y2 = 6
 
 Console.WriteLine();
 Console.WriteLine("=====================================================================");
 Console.WriteLine();
+
+
+
+
 
 // 4- Use Math.Round to round a double number to the nearest integer
 
@@ -64,6 +78,19 @@ double number = Convert.ToDouble(Console.ReadLine());
 int rounded_number = Convert.ToInt32(Math.Round(number));
 
 Console.WriteLine("Rounded number is: " + rounded_number);
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine();
+Console.WriteLine("=====================================================================");
+Console.WriteLine();
 
 
 
@@ -87,6 +114,19 @@ Console.WriteLine($"Result: {result_number}");
 
 
 
+
+
+
+Console.WriteLine();
+Console.WriteLine("=====================================================================");
+Console.WriteLine();
+
+
+
+
+
+
+
 // 6-Write a program to check whether a year is a leap or not.
 
 
@@ -99,12 +139,13 @@ bool is_Leap_Year = false;
 if (year % 2 != 0)
 { is_Leap_Year = false; }
 
-else {
+else
+{
 
     if (year % 100 == 0)
-        
-    { 
-       if (year % 400 == 0) 
+
+    {
+        if (year % 400 == 0)
         { is_Leap_Year = true; }
 
 
@@ -114,7 +155,7 @@ else {
 
     else { is_Leap_Year = (year % 4 == 0); }
 }
-if (is_Leap_Year) 
+if (is_Leap_Year)
 { Console.WriteLine($"{year} is a leap year."); }
 
 else { Console.WriteLine($"{year} is not a leap year."); }
@@ -123,19 +164,35 @@ else { Console.WriteLine($"{year} is not a leap year."); }
 
 
 
+Console.WriteLine();
+Console.WriteLine("=====================================================================");
+Console.WriteLine();
+
+
+
 
 //7- Write program to Check whether an alphabet is lowercase or uppercase
 
-        Console.Write("Please enter a character: ");
-        char ch = Convert.ToChar(Console.ReadLine());
+Console.Write("Please enter a character: ");
+char ch = Convert.ToChar(Console.ReadLine());
 
-        string result = char.IsUpper(ch)
-                        ? $"{ch} is an uppercase letter."
-                        : char.IsLower(ch)
-                        ? $"{ch} is a lowercase letter."
-                        : $"{ch} is not an alphabet character.";
+string result = char.IsUpper(ch)
+                ? $"{ch} is an uppercase letter."
+                : char.IsLower(ch)
+                ? $"{ch} is a lowercase letter."
+                : $"{ch} is not an alphabet character.";
 
-        Console.WriteLine(result);
+Console.WriteLine(result);
+
+
+
+
+
+Console.WriteLine();
+Console.WriteLine("=====================================================================");
+Console.WriteLine();
+
+
 
 
 
@@ -143,11 +200,11 @@ else { Console.WriteLine($"{year} is not a leap year."); }
 
 Console.WriteLine("Hello in my calculator ");
 Console.WriteLine("Enter Number one ");
-double num01=Convert.ToDouble( Console.ReadLine());
+double num01 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Enter Number two ");
 double num02 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("please select any operator of them : (+ - * / )");
-char ch_operator=Convert.ToChar(Console.ReadLine());
+char ch_operator = Convert.ToChar(Console.ReadLine());
 
 switch (ch_operator)
 {
@@ -171,12 +228,13 @@ switch (ch_operator)
         }
 
     case '/':
-        { if (num02 != 0) 
-            { Console.WriteLine($"{num01} / {num02} = {num01 / num02}");}
-        
+        {
+            if (num02 != 0)
+            { Console.WriteLine($"{num01} / {num02} = {num01 / num02}"); }
+
             else { Console.WriteLine("Error: Cannot divide by zero."); }
-            
-            break; 
+
+            break;
         }
 
 
@@ -193,21 +251,48 @@ switch (ch_operator)
 
 
 
+Console.WriteLine();
+Console.WriteLine("=====================================================================");
+Console.WriteLine();
+
+
+
+
+
+
+
 //9-Write a program to take 3 numbers from the keyboard and find the maximum number using ternary operator.
 
 Console.WriteLine("Enter any three numbers :");
 
-double number01=Convert.ToDouble( Console.ReadLine());
+double number01 = Convert.ToDouble(Console.ReadLine());
 
 double number02 = Convert.ToDouble(Console.ReadLine());
 
 double number03 = Convert.ToDouble(Console.ReadLine());
 
-double max = number01 > number02 
-    ?(number01 > number03 ?number01 : number03)
-    :(number02 > number03 ?  number02 : number03);
+double max = number01 > number02
+    ? (number01 > number03 ? number01 : number03)
+    : (number02 > number03 ? number02 : number03);
 
 Console.WriteLine($"The maximum number is: {max}");
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine();
+Console.WriteLine("=====================================================================");
+Console.WriteLine();
+
+
+
+
 
 
 
@@ -221,7 +306,6 @@ Console.WriteLine("Enter any number to check if it is odd or even: :");
 int numb = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(numb % 2 == 0 ? "This is an even number" : "This is an odd number");
-
 
 
 
